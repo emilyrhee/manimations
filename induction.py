6f10f1e07f1b90338ee4a8c9d06d0633a48ba48c
emilyrhee <emilyrhee99@gmail.com>
2025-05-21 19:21:03 -0400

    thinkin face and starting negative induckshun

--- induction.py
+++ induction.py
@@ -2,7 +2,7 @@ from manim import *
 
 class Induction(Scene):
     def construct(self):
-        steps : Text = Text("Steps to induction:", 40)
+        steps : Text = Text("Steps to induction:", font_size=40)
         font_size : int = 36
         p_of_n : Text = Text("Claim P(n)", font_size)
         basis : Text = Text("Show basis P(1)", font_size)
@@ -18,3 +18,27 @@ class Induction(Scene):
         for text in group:
             self.play(FadeIn(text))
             self.wait(1)
+
+        self.wait(1)
+
+        p_of_k_minus_1 : Text = Text("Show P(k-1)?", font_size)
+        p_of_k_minus_1.move_to(induction.get_center())
+
+        image : ImageMobject = ImageMobject("assets/think.png")
+        image.shift(RIGHT * 4, DOWN * 2)
+        image.scale(0.1,)
+
+        self.play(
+            Transform(induction, p_of_k_minus_1),
+            FadeIn(image)
+        )
+        self.wait(2)
+
+class NegativeInduction(Scene):
+    def construct(self):
+        p_of_n : Tex = Tex(
+            "Claim: $2n \leq n^2$ for all negative $n$.", 
+            font_size=40
+        )
+        self.play(p_of_n.animate.to_corner(UL))
+        self.wait(1)
