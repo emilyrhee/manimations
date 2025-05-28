13248f0ebc0aff66587b5d25f8fe4ad6177bfc64
emilyrhee <emilyrhee99@gmail.com>
2025-05-27 20:40:16 -0400

    four = "4"

--- induction.py
+++ induction.py
@@ -59,13 +59,29 @@ class PositiveInduction(Scene):
         self.play(FadeIn(base_case))
         self.wait(1)
         self.play(base_case.animate.shift(LEFT * 5, UP * 2))
+        self.wait(1)
+
+        left_hand_side: MathTex = MathTex("2(2) = 4", color=BLUE)
+        right_hand_side: MathTex = MathTex("2^2 = 4", color=ORANGE)
+        group: Group = Group(left_hand_side, right_hand_side)
+        group.arrange(RIGHT, buff=1)
+        for expression in group:
+            self.play(Write(expression))
+            self.wait(1)
+        
+        result: MathTex = MathTex("4", " ", "\leq", " ", "4")
+        result.shift(DOWN)
+        result[0].set_color(BLUE)
+        result[4].set_color(ORANGE)
+        check_emoji: ImageMobject = ImageMobject("assets/check.png")
+        check_emoji.scale(0.4).shift(RIGHT * 4, DOWN)
+        self.play(Write(result), FadeIn(check_emoji))
 
 class NegativeInduction(Scene):
     def construct(self):
         p_of_n: Tex = Tex(
             "Claim: ", "$2n$", " ", "$\leq$", " ", "$n^2$",
             " for all negative $n$.",
-            font_size=40
         )
         p_of_n.set_color_by_tex("2n", BLUE).set_color_by_tex("n^2", ORANGE)
         self.play(FadeIn(p_of_n))
@@ -82,12 +98,10 @@ class NegativeInduction(Scene):
 
         left_hand_side: MathTex = MathTex(
             "2(-1) = -2",
-            font_size=40,
             color=BLUE
         )
         right_hand_side: MathTex = MathTex(
             "(-1)^2 = 1",
-            font_size=40,
             color=ORANGE
         )
         group: Group = Group(left_hand_side, right_hand_side)
